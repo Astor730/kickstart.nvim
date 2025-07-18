9948dbac55585b8d7898c6e6566942596ab3fab2
Astor730 <mister.p730@gmail.com>
2025-07-18 13:58:51 -0400

    barbar is now offset by neo-tree

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -10,14 +10,18 @@ return {
   lazy = false,
   config = function()
     local map = vim.api.nvim_set_keymap
-    require('bufferline').setup {
-      options = {
-        clickable = false,
-        autohide = 1,
-      },
-    }
     local opts = { noremap = true, silent = true }
     map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
     map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+    require('barbar').setup {
+      -- this is where options are set
+      animation = true,
+      sidebar_filetypes = {
+        ['neo-tree'] = {
+          event = 'BufWinLeave',
+        },
+      },
+      non_name_title = 'Empty File',
+    }
   end,
 }
